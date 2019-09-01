be0769763e3b6a68d9a335630829dd0206fb08b6
Han <han.lai321@gmail.com>
2019-09-01 16:26:37 -0400

    update snapshot

--- __tests__/integration/schema/defaultOptions.test.js
+++ __tests__/integration/schema/defaultOptions.test.js
@@ -5,7 +5,7 @@ test(
   "prints a schema with the filter plugin",
   core.test(["p"], {
     skipPlugins: [PgConnectionArgCondition],
-    appendPlugins: [require("../../../index.js")],
+    // appendPlugins: [require("../../../index.js")],
     disableDefaultMutations: true,
     legacyRelations: "omit",
   })

b7192e2fffe66c2fe26bbaac0460a7d3dba1c5a7
Han Lai <han.lai321@gmail.com>
2019-10-02 17:46:37 -0400

    better types

--- src/postgraphile_types.ts
+++ src/postgraphile_types.ts
@@ -1,20 +1,41 @@
 import { Build } from 'postgraphile';
-import { PgAttribute, PgProc, PgConstraint, PgClass } from 'graphile-build-pg';
+import {
+  PgAttribute,
+  PgProc,
+  PgClass,
+  PgConstraint,
+  PgExtension,
+  PgType,
+  PgNamespace,
+} from 'graphile-build-pg';
+import { PgIndex } from 'graphile-build-pg/node8plus/plugins/PgIntrospectionPlugin';
 
 export type GraphilePgAttribute = PgAttribute;
 export type GraphilePgProcedure = PgProc;
-export interface GraphilePgConstraint extends PgConstraint{
+
+export interface GraphilePgConstraint extends Omit<PgConstraint, 'foreignClass' | 'class'> {
   foreignClass: GraphilePgClass;
+  class: GraphilePgClass;
 }
-export interface GraphilePgClass extends PgClass{
-  constraints: [GraphilePgConstraint];
+export interface GraphilePgClass extends Omit<PgClass, 'constraints' | 'attributes'> {
+  constraints: GraphilePgConstraint[];
+  attributes: PgAttribute[];
 }
 export interface GraphilePgIntrospection {
-  classById: {[x: string]: GraphilePgClass};
-  constraint: GraphilePgConstraint[];
+  __pgVersion: number;
+  attribute: PgAttribute[];
+  attributeByClassIdAndNum: { [classId: string]: { [num: string]: PgAttribute } };
   class: GraphilePgClass[];
-  procedure: GraphilePgProcedure[];
-  [x: string]: any;
+  classById: { [x: string]: GraphilePgClass };
+  constraint: GraphilePgConstraint[];
+  extension: PgExtension[];
+  extensionById: { [x: string]: PgExtension };
+  index: PgIndex[];
+  namespace: PgNamespace[];
+  namespaceById: { [x: string]: PgNamespace };
+  procedure: PgProc[];
+  type: PgType[];
+  typeById: { [x: string]: PgType };
 }
 export interface GraphileBuild extends Build {
   pgIntrospectionResultsByKind: GraphilePgIntrospection;

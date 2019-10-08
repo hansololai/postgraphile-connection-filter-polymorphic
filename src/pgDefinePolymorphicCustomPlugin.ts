194e7d243d80442e3e1cba1ea9eb5e0d0236497f
Han Lai <han.lai321@gmail.com>
2019-10-08 15:57:50 -0400

    also support materialized view

--- src/pgDefinePolymorphicCustomPlugin.ts
+++ src/pgDefinePolymorphicCustomPlugin.ts
@@ -37,7 +37,8 @@ export const definePolymorphicCustom = (builder: SchemaBuilder, options: Options
 
     const { pgSchemas = [] } = options as any;
     const pgPolymorphicClassAndTargetModelsCustome: PgPolymorphicConstraints = pgClasses
-      .filter(c => pgSchemas.includes(c.namespaceName) && ['r', 'v'].includes(c.classKind))
+      // get all the class that are table, view, materialized view
+      .filter(c => pgSchemas.includes(c.namespaceName) && ['r', 'v', 'm'].includes(c.classKind))
       .reduce((acc: PgPolymorphicConstraints, curClass) => {
         const curClassAttributes: { [x: string]: any } = attributeByClassIdAndNum[curClass.id];
         // We do it in two steps, first find all xxx_type

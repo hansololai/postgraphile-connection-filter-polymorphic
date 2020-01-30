fb4cf6f86a02705964fb893c9704c17572751138
Han Lai <han.lai321@gmail.com>
2020-01-30 00:34:56 -0500

    Update defaultOptions.test.js

--- __tests__/integration/schema/defaultOptions.test.js
+++ __tests__/integration/schema/defaultOptions.test.js
@@ -4,7 +4,7 @@ const { PgConnectionArgCondition } = require("graphile-build-pg");
 test(
   "prints a schema with the filter plugin",
   core.test(["p"], {
-    skipPlugins: [PgConnectionArgCondition],
+    // skipPlugins: [PgConnectionArgCondition],
     // appendPlugins: [require("../../../index.js")],
     disableDefaultMutations: true,
     legacyRelations: "omit",
